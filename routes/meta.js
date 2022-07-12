2755968ec0bcd6c5385c4fdbfa15ade3d51ed425
Exxempt <46376436+Exxempt@users.noreply.github.com>
2022-07-12 16:30:44 -0500

    change vercel config

--- routes/meta.js
+++ routes/meta.js
@@ -1,12 +1,12 @@
 var router = require('express').Router();
 const router = express.Router()
 const { apes } = require('../src/apes.js')
-
+/*
 router.get('/', function(req, res) {
     res.send('Go away');
 });
-
-router.get('/v1', function(req, res) {
+*/
+router.get('/', function(req, res, next) {
     const { name, image, meta, edition, BACKGROUND, FUR, CLOTHES, EYES, MOUTH, HATS, EARRING, BACKGROUND_Rarity, FUR_Rarity, CLOTHES_Rarity, EYES_Rarity, MOUTH_Rarity, HATS_Rarity, EARRING_Rarity, Total_Statistical_Rarity, token, rank } = req.query;
     let results = [...apes];
     if (name) {
