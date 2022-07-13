5d83a21d3d62d021a3e1b478f06370c4b6721903
Exxempt <46376436+Exxempt@users.noreply.github.com>
2022-07-12 22:19:35 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,6 @@ const User = require('../modules/user')
 router.get('/', async (req, res) => {
     try {
         const users = await User.find()
-        res.setHeader('Access-Control-Allow-Credentials', true)
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        // another common pattern
-        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.setHeader(
-          'Access-Control-Allow-Headers',
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
         res.json(users)
     }
     catch (err) {
